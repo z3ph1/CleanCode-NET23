4c8dc34afaaead2c2420e739021dcc3bfc375fba
Seph Heravi <seph.heravi@greatit.se>
2024-11-14 23:43:12 +0100

    solid content

---
+++ assets/Samples/Patterns/Current/BehavioralPatterns/Memento/IDbCommand.cs
@@ -0,0 +1,15 @@
+﻿// Copyright Information
+// =============================
+// BehavioralPatterns - IDbCommand.cs
+// All samples copyright Philip Japikse 
+// http://www.skimedic.com 20/06/2017
+// See License.txt for more information
+// =============================
+namespace BehavioralPatterns.Memento
+{
+    public interface IDbCommand
+    {
+        void Execute(string text);
+        void UnExecute();
+    }
+}
