6c987c4f3b3db83a1adfafdfc8521450fcd7591e
Seph Heravi <seph.heravi@greatit.se>
2024-12-03 17:30:51 +0100

    ms

--- assets/Samples/Microservices/Microservices/Models/Customer.cs
+++
@@ -1,8 +0,0 @@
-namespace Microservices
-{
-    public class Customer
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- assets/Samples/Microservices/Microservices/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-namespace Microservices
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-    }
-}
